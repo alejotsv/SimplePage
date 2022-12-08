6e2e2f3907be993fc1842a6df1f7e7c2b48ddaee
Alejandro Salas <asalas.wd@outlook.com>
2022-12-07 21:22:22 -0500

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import logo from '../assets/react.png';
 
 const Header = () => {
   return(
-    <header>
-      This is my header
-    </header>
+    <header className="header-container">
+    <div className="header-div-container">
+      <img className="img-logo" src={logo} alt="react logo" />
+      <h2 className="header-title">ReactFacts</h2>
+    </div>
+    <h2 className="header-right">React course - Project 1</h2>
+  </header>
   )
 }
 

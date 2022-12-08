ffa01fe5ddf54e6b05a7c765f57793de9d659746
Alejandro Salas <asalas.wd@outlook.com>
2022-12-07 21:25:15 -0500

    complete MainContent and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import reactLogo from './assets/react.png';
-import Header from "./components/Header";
+import Header from './components/Header';
+import MainContent from './components/MainContent';
 import './App.css';
 
 const App = () => { 
   return (
     <div>
       <Header />
+      <MainContent />
     </div>
   )
 }
